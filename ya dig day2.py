a6aed77736bdd5426e8930296887af98667215f3
Venisha Patel <51681164+venisha415@users.noreply.github.com>
2019-06-11 10:41:04 -0700

    Add files via upload

---
+++ ya dig day2.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+'''
+Spyder Editor
+
+This is a temporary script file.
+'''
+
+5/8 #division
+
+print("Hello",5,"World")
+
+print("ya digggg")
+
+5*2 #multipilication
+5/2 #division
+5-2 #subtraction
+5**2 
+5*2 #multiplication
+(8/9)*3 #division #multiplication
+
+#writing code to sum any two numbers
+
+def sum(v,p,c):
+    sum = v + p +c
+    print("The sum of", v, "and", p, "and", c, "is", sum)
+    
+sum(4,5,8)
+sum(3.6,4.9,8.6)
+
+#calculate the area of a trianle
+
+def trianglearea (a,b):
+    trianglearea = a*b*0.5
+    print("The area of a triangle is", trianglearea)
+
+trianglearea(3,4)
+
+def name(a):
+    name = a
+    print(" My name is", name)
+name ("Venisha")    
