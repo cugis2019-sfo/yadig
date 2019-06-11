7f514d34da3f2ca113f269f0fc652f97c75cfeaa
Venisha Patel <51681164+venisha415@users.noreply.github.com>
2019-06-11 15:26:00 -0700

    Add files via upload

---
+++ ya dig day 2 part 2.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+'''
+Spyder Editor
+
+This is a temporary script file.
+'''
+
+#day one
+
+5/8 #division
+
+print("Hello",5,"World")
+
+print("ya digggg")
+
+5*2 #multipilication
+5/2 #division
+5-2 #subtraction
+5**2 
+5*2 #multiplication
+(8/9)*3 #division #multiplication
+
+#writing code to sum any two numbers
+
+def sum(v,p,c):
+    sum = v + p +c
+    print("The sum of", v, "and", p, "and", c, "is", sum)
+    
+sum(4,5,8)
+sum(3.6,4.9,8.6)
+
+#calculate the area of a trianle
+
+def trianglearea (a,b):
+    trianglearea = a*b*0.5
+    print("The area of a triangle is", trianglearea)
+
+trianglearea(3,4)
+
+#day two
+
+def name(a):
+    name = a
+    print(" My name is", name)
+name ("Venisha")    
+
+import math
+dir(math)
+
+math.pi
+math.e
+math.sqrt(2)
+math.factorial(1730)
+math.pow(3,10)
+math.remainder(4,6)
+
+def cube(a):
+    cube = a 
+    print("the cube room of", cube)
+    
+math.isclose(2.345, 2.36)
+
+#ask user to enter value
+def user():
+    print("please select a number for t")
+    
+user()  
